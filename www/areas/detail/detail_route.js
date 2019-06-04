79a26b353beb817aa4c855569fa3167c396e628b
white wang <1476573881@qq.com>
2019-06-04 14:11:47 +0800

    最新提交更改了首页

---
+++ www/areas/detail/detail_route.js
@@ -0,0 +1,14 @@
+/* 
+*贴子详情页面的子路由模块
+*/
+
+angular.module('detail.route',['detail.controller'])
+
+.config(function($stateProvider){
+  $stateProvider
+   .state('detail',{
+     url:'/detail',
+     templateUrl:'areas/detail/detail.html',
+     controller:'DetailController'
+   })
+})
--- www/areas/home/home_controller.js
+++ www/areas/home/home_controller.js
@@ -4,8 +4,6 @@
 angular.module('home.controller',[])
 
 .controller('HomeController',function($scope,$window){
-    
-    goTop();
 
     /* 点赞设置 */
     $scope.clickIcon = function($event,iconName){   
@@ -20,21 +18,4 @@ angular.module('home.controller',[])
         }
         $event.currentTarget.className = buttonClasses;
     }
-    //回到顶部
-    function goTop(){
-        var bg = window.document.getElementById('home-content');
-        var goTop = document.querySelector(".back_top");
-        bg.addEventListener('scroll',function(){
-            var top = bg.scrollTop;
-            if(top>200){
-                goTop.style.opacity = 1;
-            }else{
-                goTop.style.opacity = 0;
-            }
-
-        },false);
-        goTop.onclick = function(){
-            bg.scrollTop = 0;
-        }
-    };
 })
