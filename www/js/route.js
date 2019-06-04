7e2b03ce6d10ac999f397b33fd883e90a5592328
CPY“ <2236808936@qq.com>
2019-06-04 14:39:20 +0800

    cpy整合后的更改

--- www/js/route.js
+++ www/js/route.js
@@ -2,11 +2,7 @@
 *项目的全局路由模块，实现启动默认页面的跳转，以及依赖子模块的路由。
 */
 
-<<<<<<< HEAD
-angular.module('route',['guidePage.route','tab.route','home.route','massage.route','publish.route'])
-=======
-angular.module('route',['guidePage.route','tab.route','home.route','publish.route','detail.route'])
->>>>>>> 79a26b353beb817aa4c855569fa3167c396e628b
+angular.module('route',['guidePage.route','tab.route','home.route','massage.route','publish.route','detail.route'])
 
 .config(function($urlRouterProvider){
   //使用H5提供的LocalStorage来存储一个布尔变量，判断是否第一次打开APP，默认值是false
