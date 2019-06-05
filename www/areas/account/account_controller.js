f11209585a16e3ac2861a1c256c42ff0e70d3c36
HH <2531180040@qq.com>
2019-06-05 14:49:06 +0800

    hh

---
+++ www/areas/account/account_controller.js
@@ -0,0 +1,10 @@
+/* 
+*我的标签页的控制器模块
+*/
+
+angular.module('account.controller',[])
+
+.controller('AccountController',function($scope){
+  $scope.test = 0
+  $scope.test1 = 1
+})
---
+++ www/areas/steam/steam_service.js
@@ -0,0 +1,13 @@
+/* 
+* steam的服务器模块
+*/
+
+angular.module('steam.service',[])
+
+.factory('SteamService',function(){
+  return{
+    getData:function(){
+      
+    }
+  }
+})
