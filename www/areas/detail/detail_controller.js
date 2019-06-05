0f7c3439a0ceef8855ee894623a6a0f8c41f5d8b
white wang <1476573881@qq.com>
2019-06-05 15:51:08 +0800

    更改了贴子详情页，缺回复

--- www/areas/detail/detail_controller.js
+++ www/areas/detail/detail_controller.js
@@ -4,6 +4,10 @@
 
 angular.module('detail.controller',[])
 
-.controller('DetailController',function($scope){
-  
+.controller('DetailController',function($scope,$location,$anchorScroll){
+  /* 设置a标签的锚点跳转 */
+  $scope.toComment = function(location) {
+    var location = $location.hash(location);
+    $anchorScroll();
+   };
 })
--- www/areas/home/home_controller.js
+++ www/areas/home/home_controller.js
@@ -1,9 +1,9 @@
 /**
  * 首页控制器模块
  */
-angular.module('home.controller',[])
+var app = angular.module('home.controller',[]);
 
-.controller('HomeController',function($scope,$window){
+app.controller('HomeController',function($scope,$window){
 
     /* 点赞设置 */
     $scope.clickIcon = function($event,iconName){   
@@ -19,3 +19,35 @@ angular.module('home.controller',[])
         $event.currentTarget.className = buttonClasses;
     }
 })
+
+app.directive('focusInput', ['$ionicScrollDelegate', '$window', '$timeout', '$ionicPosition', function ($ionicScrollDelegate, $window, $timeout, $ionicPosition) {
+    return {
+      template: '<input type="tel" maxlength="20" autocomplete="true" />',
+      restrict: 'A',
+      scope: false,
+      link: function ($scope, iElm, iAttrs, controller) {
+        if (ionic.Platform.isANDROID()) {
+            iElm.on('focus', function () {
+                var top = $ionicScrollDelegate.getScrollPosition().top;
+                var eleTop = ($ionicPosition.offset(iElm).top) / 2
+                var realTop = eleTop + top;
+                $timeout(function () {
+                    if (!$scope.$last) {
+                        $ionicScrollDelegate.scrollTo(0,realTop);
+                    } else {
+                      try {
+                          var aim = angular.element(document).find('.scroll')
+                          aim.css('transform', 'translate3d(0px,' + '-' + realTop + 'px, 0px) scale(1)');
+                          $timeout(function () {
+                              iElm[0].focus();
+                              console.log(2);
+                          }, 100)
+                      } catch (e) {
+                      }
+                    }
+                }, 500)
+            })
+        }
+      }
+    }
+}])
