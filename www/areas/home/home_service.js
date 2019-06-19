07c4fa3299968c48d99b6b8d735c1d2cd464ba5e
white wang <1476573881@qq.com>
2019-06-19 19:50:23 +0800

    更改了主页贴子信息

--- www/areas/home/home_service.js
+++ www/areas/home/home_service.js
@@ -4,10 +4,15 @@
 
 angular.module('home.service',[])
 
-.factory('homeService',function(){
+.factory('homeService',function($http,configService){
   return{
-    getData:function(){
-      
+    getData:function(callback){
+      //访问贴子信息数据的接口
+      var url = configService.getHostUrl() + '/post/selectAll.json?callback=JSON_CALLBACK'
+      $http.jsonp(url)
+        .success(function(data){
+          callback(data)
+        })
     }
   }
 })
