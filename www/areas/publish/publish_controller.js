953791985b5b2d467abbc42f18d606c6d07f2fab
HH <2531180040@qq.com>
2019-06-23 14:46:09 +0800

    hh

--- www/areas/publish/publish_controller.js
+++ www/areas/publish/publish_controller.js
@@ -6,12 +6,17 @@
 angular.module('publish.controller',[])
 
 
-.controller('PublishController',function($scope,$ionicHistory){
+.controller('PublishController',function($scope,$ionicHistory,$location){
     //返回上一页
     $scope.goBack = function(){
         $ionicHistory.goBack();
     }
-
+    $scope.toplate = function(){
+        $location.path("/plate")
+    }
+    $scope.test = function(){
+        alert("zz")
+    }
     //mydate中设置图像宽度与高度一致
     $(function (){
         var imgWidth = $("#imgWidth").innerHeight();
